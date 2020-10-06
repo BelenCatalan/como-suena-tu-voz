2ffa362bcf0676d19a0adf960587b4ce1e7111ce
BelenCatalan <labdelante@gmail.com>
2020-10-06 17:43:17 +0200

    install new gulp, problems with old starter

--- gulpfile.js
+++ gulpfile.js
@@ -16,17 +16,17 @@ const uglify = require('gulp-uglify-es').default;
 
 // secondary tasks
 
-gulp.task('api', (done) => {
+gulp.task('api', done => {
   gulp.src(config.api.src).pipe(gulp.dest(config.api.dest));
   done();
 });
 
-gulp.task('api-dist', (done) => {
+gulp.task('api-dist', done => {
   gulp.src(config.api.src).pipe(gulp.dest(config.api.dist));
   done();
 });
 
-gulp.task('bs-reload', (done) => {
+gulp.task('bs-reload', done => {
   browserSync.reload();
   done();
 });
@@ -35,7 +35,7 @@ gulp.task('clean', del.bind(null, [config.env.dev.dest]));
 
 gulp.task('clean-dist', del.bind(null, [config.env.producction.dest]));
 
-gulp.task('css', (done) => {
+gulp.task('css', done => {
   gulp
     .src(config.css.src)
     .pipe(sourcemaps.init())
@@ -49,7 +49,7 @@ gulp.task('css', (done) => {
   done();
 });
 
-gulp.task('css-dist', (done) => {
+gulp.task('css-dist', done => {
   gulp
     .src(config.css.src)
     .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
@@ -60,17 +60,17 @@ gulp.task('css-dist', (done) => {
   done();
 });
 
-gulp.task('html', (done) => {
+gulp.task('html', done => {
   gulp.src(config.html.src).pipe(htmlInclude()).pipe(gulp.dest(config.html.dest));
   done();
 });
 
-gulp.task('html-dist', (done) => {
+gulp.task('html-dist', done => {
   gulp.src(config.html.src).pipe(htmlInclude()).pipe(gulp.dest(config.html.dist));
   done();
 });
 
-gulp.task('images', (done) => {
+gulp.task('images', done => {
   gulp
     .src(config.images.src)
     .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
@@ -78,15 +78,7 @@ gulp.task('images', (done) => {
   done();
 });
 
-gulp.task('sounds', (done) => {
-  gulp
-    .src(config.sounds.src)
-    .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
-    .pipe(gulp.dest(config.sounds.dest));
-  done();
-});
-
-gulp.task('js', (done) => {
+gulp.task('js', done => {
   gulp
     .src(config.js.src)
     .pipe(sourcemaps.init())
@@ -98,7 +90,7 @@ gulp.task('js', (done) => {
   done();
 });
 
-gulp.task('js-dist', (done) => {
+gulp.task('js-dist', done => {
   gulp
     .src(config.js.src)
     .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
@@ -108,7 +100,7 @@ gulp.task('js-dist', (done) => {
   done();
 });
 
-gulp.task('images-dist', (done) => {
+gulp.task('images-dist', done => {
   gulp
     .src(config.images.src)
     .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
@@ -116,24 +108,15 @@ gulp.task('images-dist', (done) => {
   done();
 });
 
-gulp.task('sounds-dist', (done) => {
-  gulp
-    .src(config.sounds.src)
-    .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
-    .pipe(gulp.dest(config.sounds.dist));
-  done();
-});
-
 // main tasks
 
 gulp.task(
   'default',
-  gulp.series(['clean', 'api', 'html', 'css', 'js', 'images', 'sounds'], (done) => {
+  gulp.series(['clean', 'api', 'html', 'css', 'js', 'images'], done => {
     browserSync.init({ server: { baseDir: './public/' } });
     gulp.watch(config.api.src, gulp.series(['api', 'bs-reload']));
     gulp.watch(config.css.src, gulp.series('css'));
     gulp.watch(config.images.src, gulp.series(['images', 'bs-reload']));
-    gulp.watch(config.sounds.src, gulp.series(['sounds', 'bs-reload']));
     gulp.watch(config.js.src, gulp.series(['js', 'bs-reload']));
     gulp.watch(config.watch.html, gulp.series(['html', 'bs-reload']));
     done();
@@ -149,10 +132,9 @@ gulp.task(
       'css-dist',
       'html-dist',
       'js-dist',
-      'images-dist',
-      'sounds-dist',
+      'images-dist'
       // 'icons-dist'
     ],
-    (done) => done()
+    done => done()
   )
 );
